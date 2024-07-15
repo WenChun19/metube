7ae10b5f66633b835a28eb6e2f1a44b2f5bcf46a
wc.hoh <hohwenchun96@gmail.com>
2024-07-15 21:26:08 +0800

    Add category pills and selected category

---
+++ src/components/CategoryPills.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Button from "./Button";
+
+type CategoryPillProps = {
+  categories: string[];
+  selectedCategory: string;
+  onSelect: (category: string) => void;
+};
+
+const CategoryPills = ({
+  categories,
+  selectedCategory,
+  onSelect,
+}: CategoryPillProps) => {
+  return (
+    <div className="overflow-x-hidden relative">
+      <div
+        className="flex whitespace-nowrap gap-3 
+      transition-transform w-[max-content]"
+      >
+        {categories.map((category) => (
+          <Button
+            key={category}
+            variant={selectedCategory === category ? "dark" : "default"}
+            className="py-1 px-3 rounded-lg whitespace-nowrap"
+            onClick={() => onSelect(category)}
+          >
+            {category}
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPills;
---
+++ src/data/home.ts
@@ -0,0 +1,14 @@
+export const categories = [
+    "All",
+    "Javascript",
+    "Typescript",
+    "Programming",
+    "Weight Lifting",
+    "Bowling",
+    "Queens Of Tears",
+    "React",
+    "Laowu",
+    "Next.js",
+    "Coding",
+    "Web Development"
+]
